50e4dcebd8042d230d84895cd50ebdc45ffb54d0
Curious-DevlOper <elham.bolandi@gmai.com>
2023-08-12 21:03:14 +0200

    inject service inside another service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,8 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable() //add some metadata to inject service
+
 export class AccountsService {
   accounts = [
       {
@@ -13,9 +18,13 @@ export class AccountsService {
         status: 'Unknown'
       }
     ];
+    // using service inside another service
+    constructor(private loggingService: LoggingService){}
+
     //push new account int array
   addAccount(name: string, status:string) {
       this.accounts.push({name: name, status: status});
+      this.loggingService.logStatusChange(status);
   }
   updateAccount(id: number, status:string) {
       this.accounts[id].status = status;
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
@@ -15,8 +15,8 @@ export class NewAccountComponent {
 
   }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountsService.addAccount(accountName, accountStatus),
-    this.loggingService.logStatusChange(accountStatus);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    this.accountsService.addAccount(accountName, accountStatus)
+    // this.loggingService.logStatusChange(accountStatus);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
   }
 
